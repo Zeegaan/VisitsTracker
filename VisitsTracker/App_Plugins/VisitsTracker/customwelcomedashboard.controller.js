fa648f26d766727eb3a7b533b7e8f7b253703755
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-07-25 12:52:43 +0200

    Remove obsolete user from controller

--- VisitsTracker/App_Plugins/VisitsTracker/customwelcomedashboard.controller.js
+++ VisitsTracker/App_Plugins/VisitsTracker/customwelcomedashboard.controller.js
@@ -1,11 +1,6 @@
 angular.module("umbraco").controller("CustomWelcomeDashboardController", function ($scope, userService, trackingResource) {
     var vm = this;
-    vm.UserName = "guest";
     vm.Trackings = [];
-
-    var user = userService.getCurrentUser().then(function(user) {
-        vm.UserName = user.name;
-    });
 
     trackingResource.getAll().then(function (response){
         vm.Trackings = response;
